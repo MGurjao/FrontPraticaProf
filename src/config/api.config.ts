682617f76657461ee71a7ca417e32d08dae96b97
Martinho <martinhorneto@gmail.com>
2019-06-02 11:58:39 -0300

    obtendo dados form de login

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
     baseUrl:"http://localhost:8100" //URl Banco
-    //bucketBaseUrl:"" // Bucket "s3" da amazon
+    bucketBaseUrl:"" // Bucket "s3" da amazon aula 115
 }
