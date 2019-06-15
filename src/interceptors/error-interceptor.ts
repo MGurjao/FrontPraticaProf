cf9c0c161ac9c412ac6427bd430d9323b3d55c61
Martinho <martinhorneto@gmail.com>
2019-06-15 16:06:53 -0300

    salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -35,6 +35,10 @@ export class ErrorInterceptor implements HttpInterceptor {
                 this.handle403();
                 break;
 
+                case 422:
+                this.handle422(errorObj);
+                break
+
                 default:
                 this.handleDefaultEror(errorObj);
             }
@@ -63,6 +67,20 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     }
 
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
     handleDefaultEror(errorObj){
         let alert = this.alertCtrl.create({
             title:'Erro' + errorObj.status + ': ' + errorObj.error,
@@ -76,6 +94,14 @@ export class ErrorInterceptor implements HttpInterceptor {
         });
         alert.present();
         
+    }
+
+    private listErrors(messages : FieldMessage[]) : string {
+        let s : string = '';
+        for (var i=0; i<messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+        }
+        return s;
     }
 }
 
